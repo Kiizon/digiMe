b0f8c5b58616fe0ba9daff9ed06b4b65f34808f1
kish dizon <kishddizon@gmail.com>
2025-05-15 03:27:52 -0400

    update sun and moon toggle

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.content');
+const sunIcon = document.querySelector('.theme-icon.sun');
+const moonIcon = document.querySelector('.theme-icon.moon');
+
+
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
+      document.querySelector('.tab.active').classList.remove('active');
+      tab.classList.add('active');
 
-    document.querySelector('.tab.active').classList.remove('active');
-    tab.classList.add('active');
-
-    document.querySelector('.content.active').classList.remove('active');
-    document.getElementById(tab.dataset.target).classList.add('active');
+      document.querySelector('.content.active').classList.remove('active');
+      document.getElementById(tab.dataset.target).classList.add('active');
   });
 });
+
+function toggleIcons() {
+    if (sunIcon.style.display === 'none') {
+        sunIcon.style.display = 'block';
+        moonIcon.style.display = 'none';
+    } else {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'block';
+    }
+}
